890b7b471c964b288f4a71447a6578e4d80b346b
bentoco <bentoco@outlook.com.br>
2020-09-13 14:19:41 -0300

    Database update

--- src/database/index.js
+++ src/database/index.js
@@ -1,26 +1,26 @@
-const { MongoClient } = require("mongodb");
-const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+const { MongoClient } = require('mongodb')
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env
 
 async function main() {
-  const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  try {
-    await client.connect();
-    await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    try {
+        await client.connect()
+        await listDatabases(client)
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await client.close()
+    }
 }
-main().catch(console.error);
+main().catch(console.error)
 
 async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+    const databasesList = await client.db().admin().listDatabases()
+    console.log('Databases:')
+    databasesList.databases.forEach((db) => console.log(` - ${db.name}`))
 }
