ce098f695dc39eae5d2ae9de9d0321fdd4e79641
bentoco <bentoco@outlook.com.br>
2020-09-15 22:30:51 -0300

    Mongoose connection implement

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 require('dotenv').config()
+const db = require('./src/database')
 const router = require('./routes')
 const port = process.env.PORT || 3000
 
