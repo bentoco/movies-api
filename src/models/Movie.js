cc27ecb549dfac399616b1729ccb4793b03bd091
bentoco <bentoco@outlook.com.br>
2020-09-16 18:54:55 -0300

    Stopped tracking .env File

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,31 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId
 
-const Movie = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    categories: {
-        type: Array,
-        required: true,
-    },
-    director: {
-        type: String,
-        required: true,
-    },
-    stars: {
-        type: Array,
-        required: true,
-    },
-    time: {
-        type: String,
-        required: true,
-    },
-    synopsis: {
-        type: String,
-        required: true,
-    },
-})
+const Movie = new Schema({})
 
 mongoose.model('movies', Movie)
