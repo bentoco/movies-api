0d0d51f1fd75ceb52794a03d910d21f2867dbcf7
bentoco <bentoco@outlook.com.br>
2020-09-16 21:22:10 -0300

    Mongoose connection adjust

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,18 +2,35 @@ const mongoose = require('mongoose')
 
 const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env
 
-module.exports.main = async () => {
+mongoose.Promise = Promise
+
+mongoose.connection.on('connected', () => {
+    console.log('Connection Established')
+})
+
+mongoose.connection.on('reconnected', () => {
+    console.log('Connection Reestablished')
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Connection Disconnected')
+})
+
+mongoose.connection.on('close', () => {
+    console.log('Connection Closed')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('ERROR: ' + error)
+})
+
+const run = async () => {
     const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
     const options = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }
-    try {
-        await mongoose.connect(uri, options)
-        console.log('Database connected!')
-    } catch (error) {
-        console.log('Connection error:' + error)
-    } finally {
-        await mongoose.connection.close()
-    }
+    await mongoose.connect(uri, options)
 }
+
+run().catch((error) => console.error(error))
